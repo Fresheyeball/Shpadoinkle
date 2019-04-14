10b21dcaf710201f0a6eb9767a3cf897bbfacbd5
Isaac Shapira <fresheyeball@protonmail.com>
2019-04-13 23:47:47 -0600

    everything in its place atleast

--- Shpadoinkle/Html.hs
+++
@@ -1,9 +0,0 @@
-module Shpadoinkle.Html
-  ( module Shpadoinkle.Html.Element
-  , module Shpadoinkle.Html.Event
-  , module Shpadoinkle.Html.Property ) where
-
-
-import           Shpadoinkle.Html.Element
-import           Shpadoinkle.Html.Event
-import           Shpadoinkle.Html.Property
