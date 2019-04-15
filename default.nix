2c418754a657a40eea74ecbd62f7811bce098278
Isaac Shapira <fresheyeball@protonmail.com>
2019-04-15 08:51:43 -0600

    add ignorance

--- default.nix
+++ default.nix
@@ -16,11 +16,18 @@ let
 
   ghc = pkgs.haskell.packages.ghc843;
 
+  gitignore = (pkgs.callPackage (pkgs.fetchFromGitHub
+    { owner = "siers";
+      repo = "nix-gitignore";
+      rev = "4f2d85f2f1aa4c6bff2d9fcfd3caad443f35476e";
+      sha256 = "1vzfi3i3fpl8wqs1yq95jzdi6cpaby80n8xwnwa8h2jvcw3j7kdz";
+    }) {}).gitignoreSource [".git"];
+
   targets = {
-    Shpadoinkle = ./core;
-    Shpadoinkle-backend-snabbdom = ./backends/snabbdom;
-    Shpadoinkle-backend-pardiff  = ./backends/pardiff;
-    Shpadoinkle-html = ./html;
+    Shpadoinkle = gitignore ./core;
+    Shpadoinkle-backend-snabbdom = gitignore ./backends/snabbdom;
+    Shpadoinkle-backend-pardiff  = gitignore ./backends/pardiff;
+    Shpadoinkle-html = gitignore ./html;
   };
 
   haskellPackages = ghc.extend (pkgs.lib.composeExtensions
@@ -40,7 +47,5 @@ let
 in
 
   if pkgs.lib.inNixShell
-
   then haskellPackages.shellFor { packages = _: packages; buildInputs = tools; }
-
   else buildSet
