ed08dcb8db886d94787334e907451e14fa0eb646
Isaac Shapira <fresheyeball@protonmail.com>
2020-09-29 09:58:22 -0600

    update boostrap cdn, and README.md

--- examples/servant-crud/View.hs
+++ examples/servant-crud/View.hs
@@ -255,7 +255,7 @@ template fe stage = H.html_
   [ H.head_
     [ H.link'
       [ H.rel "stylesheet"
-      , H.href "https://cdn.usebootstrap.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      , H.href "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
       ]
     , H.meta [ H.charset "ISO-8859-1" ] []
     , toHydration fe
