5ac386bff77f3b8fa809aa75bc65d73da61389c4
Isaac Shapira <fresheyeball@protonmail.com>
2020-12-21 14:36:32 -0700

    manifest by nix

--- developer-tools/default.nix
+++ developer-tools/default.nix
@@ -3,12 +3,24 @@
 }:
 let
 
+  chrome-webstore =
+    { name        = "Shpadoinkle Developer Tools";
+      description = "Time travel debugging for Shpadoinkle";
+      author      = "Isaac Shapira";
+      version     = "0.0.1";
+    };
+
   pkgs = import ../nix/pkgs.nix {
     inherit compiler chan; isJS = true; };
 
   util = import ../nix/util.nix { inherit pkgs compiler; isJS = true; };
 
   dev  = pkgs.haskell.packages.${util.compilerjs};
+
+  patch-manifest = with chrome-webstore; ''
+    ${pkgs.jq}/bin/jq '.name = "${name}" | .description = "${description}" | .version = "${version}" | .author = "${author}"' < manifest.json > manifest-tmp.json
+    mv manifest-tmp.json manifest.json
+  '';
 
 in pkgs.runCommand "Shpadoinkle-developer-tools.zip" {}
   ''
@@ -19,6 +31,7 @@ in pkgs.runCommand "Shpadoinkle-developer-tools.zip" {}
     cp ${./icon.png}      ./icon.png
     cp ${./inject.js}     ./inject.js
     cp ${./style.css}     ./style.css
+    ${patch-manifest}
     cp ${util.doCannibalize dev.Shpadoinkle-developer-tools}/bin/devtools.jsexe/all.js ./all.js
     ${pkgs.zip}/bin/zip -r $out *
   ''
