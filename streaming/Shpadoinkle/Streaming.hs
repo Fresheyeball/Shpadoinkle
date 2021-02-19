f297b6832bed65f5a93695eb7a7d947c3681b20d
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-02-19 17:32:18 -0500

    consumeStream

--- streaming/Shpadoinkle/Streaming.hs
+++ streaming/Shpadoinkle/Streaming.hs
@@ -1,19 +1,28 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+
 module Shpadoinkle.Streaming
   ( consumeStream
   ) where
 
 
-{-
- - import Shpadoinkle
+import Shpadoinkle hiding (h)
 
-import Data.Functor.Of (Of)
+import Data.Functor.Of (Of ((:>)))
 import Streaming (Stream)
 import Streaming.Internal (destroy)
 
 
-consumeStream :: Stream (Of a) m () -> (a -> 
-consumeStream = consumeStream
--}
+consumeStream :: forall m a b. Monad m => Stream (Of a) m () -> (a -> m (b -> b)) -> Continuation m b
+consumeStream stream f = destroy stream g h j
+  where
+    g :: Of a (Continuation m b) -> Continuation m b
+    g (a :> k) = voidRunContinuationT $ do
+      commit (impur (f a))
+      commit (merge k)
 
-consumeStream :: a
-consumeStream = consumeStream
+    h :: m (Continuation m b) -> Continuation m b
+    h = causedBy
+
+    j :: () -> Continuation m b
+    j = const done
