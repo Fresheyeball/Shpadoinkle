4b51584561d7cfc3e76084747271f462a61d7e46
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-02-23 14:18:24 -0500

    fill out example

--- examples/Streaming.hs
+++ examples/Streaming.hs
@@ -1,4 +1,49 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE PackageImports       #-}
+
+
 module Main where
 
+
+import             Prelude                     hiding (div)
+
+import             Control.Concurrent          (threadDelay)
+import             Control.Monad.IO.Class      (MonadIO (liftIO))
+import             Data.Text                   (Text, pack)
+import             Shpadoinkle                 (Html, liftC)
+import             Shpadoinkle.Backend.ParDiff (runParDiff)
+import             Shpadoinkle.Html            (button, div, getBody, text, onClickC)
+import             Shpadoinkle.Streaming       (consumeStream)
+import             Shpadoinkle.Run             (runJSorWarp, simple)
+import "streaming" Streaming                   (Stream, Of)
+import             Streaming.Prelude           (repeatM)
+
+default (Text)
+
+
+exampleStream :: MonadIO m => Stream (Of Int) m ()
+exampleStream = repeatM $ do
+  liftIO $ threadDelay 1000000
+  return 1
+
+
+newtype Model = Model { streamContents :: [Int] }
+  deriving (Eq, Show)
+
+
+view :: MonadIO m => Model -> Html m Model
+view (Model ns) =
+  div
+    []
+    [ text (pack (show ns))
+    , liftC (\c m -> m { streamContents = c}) streamContents $
+      button
+        [ onClickC (consumeStream exampleStream (return . (:))) ]
+        [ text "Go" ]
+    ]
+
+
 main :: IO ()
-main = putStrLn "hello world"
+main = runJSorWarp 8080 $
+  simple runParDiff (Model []) view getBody
