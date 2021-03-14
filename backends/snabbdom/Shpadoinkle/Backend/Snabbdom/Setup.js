5321cd1140f0fde284a312f77306b617d8524a74
Athan Clark <athan.clark@gmail.com>
2021-03-14 02:48:01 +0000

    Snabbdom static

--- backends/snabbdom/Shpadoinkle/Backend/Snabbdom/Setup.js
+++ backends/snabbdom/Shpadoinkle/Backend/Snabbdom/Setup.js
@@ -1,22 +1,5 @@
 (() => {
 
-const head = document.getElementsByTagName('head')[0];
-const addScript = x => {
-  const s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.src = x;
-  head.appendChild(s);
-}
-
-const cdnjs = x => "https://cdnjs.cloudflare.com/ajax/libs/snabbdom/0.7.4/" + x;
-
-addScript(cdnjs("snabbdom.min.js"));
-addScript(cdnjs("snabbdom-class.min.js"));
-addScript(cdnjs("snabbdom-props.min.js"));
-addScript(cdnjs("snabbdom-attributes.min.js"));
-addScript(cdnjs("snabbdom-eventlisteners.min.js"));
-addScript(cdnjs("h.min.js"));
-
 window.startApp = cb => setTimeout(() => {
   const patch = snabbdom.init([
     snabbdom_props.default,
