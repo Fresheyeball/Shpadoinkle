8e0efbb11857a1af47038dae07b8140291c251ed
Isaac Shapira <fresheyeball@protonmail.com>
2021-03-26 01:50:13 -0600

    prop exit code

--- nix/deploy-isreal.sh
+++ nix/deploy-isreal.sh
@@ -3,4 +3,6 @@ which nixops; nixops --version
 nixops create -d colorado isreal/deploy.nix
 hash=$(cat ./nix/chan.nix | tr -d '"')
 nixops deploy -d colorado --allow-reboot --fallback -I nixpkgs="https://github.com/NixOS/nixpkgs/archive/$hash.tar.gz"
+res=$?
 nixops destroy --all && nixops delete --all
+exit $res
--- nix/test-isreal.sh
+++ nix/test-isreal.sh
@@ -2,5 +2,7 @@ nixops destroy --all && nixops delete --all
 which nixops; nixops --version
 nixops create -d tmp isreal/deploy.nix
 hash=$(cat ./nix/chan.nix | tr -d '"')
-nixops deploy -d tmp --allow-reboot --fallback -I nixpkgs="https://github.com/NixOS/nixpkgs/archive/$hash.tar.gz"
+nixops deploy -d tmp --allow-reboot --dry-run --fallback -I nixpkgs="https://github.com/NixOS/nixpkgs/archive/$hash.tar.gz"
+res=$?
 nixops destroy --all && nixops delete --all
+exit $res
