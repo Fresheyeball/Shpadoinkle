5f0224eb111dcf0fba3cdc3a0af7712a4cf964ee
Isaac Shapira <fresheyeball@protonmail.com>
2021-06-03 21:50:37 -0600

    cachix should fail

--- nix/cachix.sh
+++ nix/cachix.sh
@@ -1,22 +1,34 @@
 # We want cache as a singular step. Because we do not wish to cache parts of the
 # project if the set is broken.
 
-nix-build --arg isJS true                              | cachix push shpadoinkle &
-nix-build --arg isJS false                             | cachix push shpadoinkle &
-nix-build --arg isJS true  -A Shpadoinkle-examples.env | cachix push shpadoinkle &
-nix-build --arg isJS false -A Shpadoinkle-examples.env | cachix push shpadoinkle &
+set -eu
 
-nix-build --argstr system x86_64-darwin --arg isJS true                              | cachix push shpadoinkle &
-nix-build --argstr system x86_64-darwin --arg isJS false                             | cachix push shpadoinkle &
-nix-build --argstr system x86_64-darwin --arg isJS true  -A Shpadoinkle-examples.env | cachix push shpadoinkle &
-nix-build --argstr system x86_64-darwin --arg isJS false -A Shpadoinkle-examples.env | cachix push shpadoinkle &
+cmds=(
+  "nix-build --arg isJS true"
+  "nix-build --arg isJS false"
+  "nix-build --arg isJS true  -A Shpadoinkle-examples.env"
+  "nix-build --arg isJS false -A Shpadoinkle-examples.env"
+  "nix-build --argstr system x86_64-darwin --arg isJS true"
+  "nix-build --argstr system x86_64-darwin --arg isJS false"
+  "nix-build --argstr system x86_64-darwin --arg isJS true  -A Shpadoinkle-examples.env"
+  "nix-build --argstr system x86_64-darwin --arg isJS false -A Shpadoinkle-examples.env"
+  "nix-build ./snowman/template --arg isJS false --arg asShell true                       --no-out-link"
+  "nix-build ./snowman/template --arg isJS true  --arg asShell true                       --no-out-link"
+  "nix-build ./snowman/template --arg isJS false --arg asShell true --arg withHoogle true --no-out-link"
+  "nix-build ./snowman/template --argstr system x86_64-darwin --arg isJS false --arg asShell true                       --no-out-link"
+  "nix-build ./snowman/template --argstr system x86_64-darwin --arg isJS true  --arg asShell true                       --no-out-link"
+  "nix-build ./snowman/template --argstr system x86_64-darwin --arg isJS false --arg asShell true --arg withHoogle true --no-out-link"
+)
 
-nix-build ./snowman/template --arg isJS false --arg asShell true                       --no-out-link | cachix push shpadoinkle &
-nix-build ./snowman/template --arg isJS true  --arg asShell true                       --no-out-link | cachix push shpadoinkle &
-nix-build ./snowman/template --arg isJS false --arg asShell true --arg withHoogle true --no-out-link | cachix push shpadoinkle &
+pids=()
 
-nix-build ./snowman/template --argstr system x86_64-darwin --arg isJS false --arg asShell true                       --no-out-link | cachix push shpadoinkle &
-nix-build ./snowman/template --argstr system x86_64-darwin --arg isJS true  --arg asShell true                       --no-out-link | cachix push shpadoinkle &
-nix-build ./snowman/template --argstr system x86_64-darwin --arg isJS false --arg asShell true --arg withHoogle true --no-out-link | cachix push shpadoinkle &
-wait || exit $?
+for cmd in "${cmds[@]}"; do
+  $cmd | cachix push shpadoinkle &
+  pids+=($!)
+done
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
+
 echo "Cachix up to date"
