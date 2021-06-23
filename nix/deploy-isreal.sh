cc4907463439510240de84eac94a270ff14f4b20
Isaac Shapira <fresheyeball@protonmail.com>
2021-06-23 14:36:11 -0600

    update deploy to use mutex

--- nix/deploy-isreal.sh
+++ nix/deploy-isreal.sh
@@ -1,8 +1,20 @@
+echo "Checking for nixops mutex"
+until ! [[ -e /tmp/nixops-mutex ]]; do
+  echo "Mutex found, waiting for release"
+  wait 1
+done
+echo "Mutex is open, locking"
+touch /tmp/nixops-mutex
+echo "Destroying any stray deployments"
 nixops destroy --all && nixops delete --all
+echo "Deploying Isreal Swan"
 which nixops; nixops --version
 nixops create -d colorado isreal/deploy.nix
 hash=$(cat ./nix/chan.nix | tr -d '"')
 nixops deploy -d colorado --allow-reboot --fallback -I nixpkgs="https://github.com/NixOS/nixpkgs/archive/$hash.tar.gz"
 res=$?
+echo "Destroying deployment"
 nixops destroy --all && nixops delete --all
+echo "Releasing lock"
+rm /tmp/nixops-mutex
 exit $res
