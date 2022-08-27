4ad35ed2991b582f1315a59df9b8b6ec2ebda885
Maynard <elimaynard923@gmail.com>
2022-08-26 17:20:14 -0700

    move Shpadoinkle.Continuation to new js ffi

--- core/Shpadoinkle/Continuation.hs
+++ core/Shpadoinkle/Continuation.hs
@@ -51,10 +51,7 @@ import           Control.Monad.Trans.Class           (MonadTrans (..))
 import           Data.Foldable                       (traverse_)
 import           Data.Maybe                          (fromMaybe)
 import           GHC.Conc                            (retry)
-import           GHCJS.DOM                           (currentWindowUnchecked)
-import           GHCJS.DOM.Window                    (Window)
-import           GHCJS.DOM.WindowOrWorkerGlobalScope (clearTimeout, setTimeout)
-import           Language.Javascript.JSaddle         (MonadJSM, fun, JSM)
+import Shpadoinkle.JSFFI (clearTimeout, setTimeout, MonadJSM, mkFun', JSM)
 import           UnliftIO                            (MonadUnliftIO, TVar,
                                                       UnliftIO, askUnliftIO,
                                                       atomically, liftIO,
@@ -427,8 +424,6 @@ shouldUpdate sun prev currentModel = do
   previousModel :: TVar a     <- newTVarIO sampleModel
   -- store the accumulating value in a TVar so we can control when it updates
   currentState  :: TVar b     <- newTVarIO prev
-  -- get the window once
-  window        :: Window     <- currentWindowUnchecked
   -- get the execution context once
   context       :: UnliftIO m <- askUnliftIO
 
@@ -445,10 +440,10 @@ shouldUpdate sun prev currentModel = do
         if new' == old then retry else new' <$ writeTVar previousModel new'
 
       -- if we already had something scheduled to run, cancel it
-      traverse_ (clearTimeout window . Just) frames
+      traverse_ clearTimeout frames
 
       -- generate a callback for the request animation frame
-      let callback = fun $ \_ _ _ -> do
+      callback <- mkFun' $ \_ -> do
              -- get the current state
              x <- readTVarIO currentState
              -- run the action against the current state, and the new model
@@ -457,10 +452,10 @@ shouldUpdate sun prev currentModel = do
              atomically $ writeTVar currentState y
              -- note this means that @newModel@ updates for each call to @go@
              -- but @currentState@ only updates if the frame is actually called
-             traverse_ (clearTimeout window . Just) frames
+             traverse_ clearTimeout frames
 
       -- schedule the action to run on the next frame
-      frameId' <- setTimeout window callback Nothing
+      frameId' <- setTimeout 0 callback
 
       go (frameId':frames)
 
--- jsffi/lib/Shpadoinkle/JSFFI.hs
+++ jsffi/lib/Shpadoinkle/JSFFI.hs
@@ -1,4 +1,122 @@
-module Shpadoinkle.JSFFI where
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE JavaScriptFFI #-}
 
-jsffi :: String
-jsffi = "pranked"
+module Shpadoinkle.JSFFI
+  ( module Shpadoinkle.JSFFI
+  , MonadJSM
+  , JSM
+  , Window
+  ) where
+
+#ifdef ghcjs_HOST_OS
+import Unsafe.Coerce (unsafeCoerce)
+import Data.Coerce (coerce)
+import Control.Category ((>>>), (<<<))
+#endif
+
+-- ghcjs imports
+import GHCJS.Types (JSVal)
+#ifdef ghcjs_HOST_OS
+import JavaScript.Array.Internal (toListIO, SomeJSArray (..))
+import GHCJS.Foreign.Callback (OnBlocked (ContinueAsync), syncCallback2, Callback)
+#endif
+
+-- Imports from packages which transitively depend on jsaddle
+--
+-- Since the purpose of this package is to replace JSaddle, so it can be removed
+-- from Shpadoinkle, these imports will eventually be removed and replaced with
+-- custom counterparts. However, while we are transitioning Shpadoinkle from JSaddle
+-- to this package, it helps for the two to share types.
+import Language.Javascript.JSaddle (JSM, MonadJSM)
+import GHCJS.DOM.Window (Window)
+#ifdef ghcjs_HOST_OS
+import Language.Javascript.JSaddle (liftJSM)
+#endif
+
+
+ghcjsOnly :: a
+ghcjsOnly = error "Attempted to perform an operation which is permitted on GHCjs only. Are you compiling with GHC? The GHC Shpadoinkle API is intended for typechecking only; you must compile to GHCjs in order to actually run anything that requires Javascript. (This is most things, unless you are using the static backend.)"
+
+
+-- nb. Unused at time of writing; remove?
+currentWindowUnsafe :: MonadJSM m => m Window
+#ifdef ghcjs_HOST_OS
+currentWindowUnsafe = liftJSM $ unsafeCoerce <$> currentWindowUnsafe_js
+foreign import javascript unsafe
+  "$r = globalThis.window"
+  currentWindowUnsafe_js :: IO JSVal
+#else
+currentWindowUnsafe = ghcjsOnly
+#endif
+
+
+newtype JSFunction = JSFunction JSVal
+
+-- Function type for Haskell/JS interpolation
+type HSFunction =
+     JSVal   -- ^ function object
+  -> JSVal   -- ^ this
+  -> [JSVal] -- ^ function args
+  -> JSM ()
+
+mkFun :: MonadJSM m => HSFunction -> m JSFunction
+#ifdef ghcjs_HOST_OS
+mkFun fun =
+  let
+    middleman :: JSVal -> JSVal -> IO ()
+    middleman this args = do
+      argsList <- toListIO (coerce args)
+      fun this this argsList
+        -- FIXME
+        -- We are passing 'this' for the function object, which is wrong
+        -- This bug is inherited from JSaddle and may be relied upon by client code
+
+  in liftJSM $ do
+       cb <- syncCallback2 ContinueAsync middleman
+       JSFunction <$> callbackToJSVal cb
+
+foreign import javascript unsafe
+  "$r = function() { $1(this, arguments); }"
+  callbackToJSVal :: Callback (JSVal -> JSVal -> IO ()) -> IO JSVal
+#else
+mkFun = ghcjsOnly
+#endif
+
+mkFun' :: MonadJSM m => ([JSVal] -> JSM ()) -> m JSFunction
+mkFun' fun = mkFun (\_fun _this args -> fun args)
+
+
+#ifdef ghcjs_HOST_OS
+-- nb I don't know if these are correct
+intToJSVal :: Int -> JSVal
+intToJSVal = fromIntegral >>> (unsafeCoerce :: Double -> JSVal)
+jsValToInt :: JSVal -> Int
+jsValToInt = (unsafeCoerce :: JSVal -> Double) >>> round
+  -- 'round' seems fishy, but that's what JSaddle uses ...
+#endif
+
+
+setTimeout :: MonadJSM m => Int -> JSFunction -> m Int
+#ifdef ghcjs_HOST_OS
+setTimeout delay (JSFunction fun) =
+    liftJSM $ jsValToInt <$> setTimeout_js fun (intToJSVal delay)
+
+foreign import javascript unsafe
+  "$r = globalThis.setTimeout($1, $2)"
+  setTimeout_js :: JSVal -> JSVal -> JSM JSVal
+#else
+setTimeout = ghcjsOnly
+#endif
+
+
+clearTimeout :: MonadJSM m => Int -> m ()
+#ifdef ghcjs_HOST_OS
+clearTimeout tid =
+    liftJSM $ clearTimeout_js (intToJSVal tid)
+
+foreign import javascript unsafe
+  "globalThis.clearTimeout($1)"
+  clearTimeout_js :: JSVal -> JSM ()
+#else
+clearTimeout = ghcjsOnly
+#endif
