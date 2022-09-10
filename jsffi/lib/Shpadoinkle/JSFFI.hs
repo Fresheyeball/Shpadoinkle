f1d6dfefe044bb4c113fec75f7ffcdfa8382ae3a
Maynard <elimaynard923@gmail.com>
2022-09-10 13:53:05 -0700

    expand jsffi

--- jsffi/lib/Shpadoinkle/JSFFI.hs
+++ jsffi/lib/Shpadoinkle/JSFFI.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP           #-}
 {-# LANGUAGE JavaScriptFFI #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Shpadoinkle.JSFFI
   ( module Shpadoinkle.JSFFI
@@ -12,11 +13,14 @@ module Shpadoinkle.JSFFI
   ) where
 
 import           Control.Monad.IO.Class      (MonadIO)
+import Control.Monad ((<=<))
+import           Unsafe.Coerce               (unsafeCoerce)
+import           System.IO.Unsafe            (unsafePerformIO)
 #ifdef ghcjs_HOST_OS
 import           Control.Category            ((<<<), (>>>))
 import           Control.Monad.IO.Class      (liftIO)
 import           Data.Coerce                 (coerce)
-import           Unsafe.Coerce               (unsafeCoerce)
+import Data.Traversable (for)
 #endif
 
 -- ghcjs imports
@@ -35,26 +39,129 @@ import           JavaScript.Array.Internal   (SomeJSArray (..), toListIO)
 -- custom counterparts. However, while we are transitioning Shpadoinkle from JSaddle
 -- to this package, it helps for the two to share types.
 import           GHCJS.DOM.Window            (Window)
-import           Language.Javascript.JSaddle (JSM, MonadJSM, liftJSM)
+import           Language.Javascript.JSaddle (JSM, MonadJSM, PToJSVal,
+                                              ToJSString, ToJSVal, liftJSM,
+                                              pToJSVal, toJSString, toJSVal)
 
 
 ghcjsOnly :: a
 ghcjsOnly = error "Attempted to perform an operation which is permitted on GHCjs only. Are you compiling with GHC? The GHC Shpadoinkle API is intended for typechecking only; you must compile to GHCjs in order to actually run anything that requires Javascript. (This is most things, unless you are using the static backend.)"
 
 
--- nb. Unused at time of writing; remove?
-currentWindowUnsafe :: MonadJSM m => m Window
+--------------------------------------------------------------------------------
+-- JSObject
+
+newtype JSObject = JSObject { unJSObject :: JSVal }
+
+mkEmptyObject :: MonadJSM m => m JSObject
 #ifdef ghcjs_HOST_OS
-currentWindowUnsafe = liftJSM $ unsafeCoerce <$> currentWindowUnsafe_js
+mkEmptyObject = JSObject <$> liftJSM mkEmptyObject_js
 foreign import javascript unsafe
-  "$r = globalThis.window"
-  currentWindowUnsafe_js :: IO JSVal
+  "$r = ({})"
+  mkEmptyObject_js :: JSM JSVal
 #else
-currentWindowUnsafe = ghcjsOnly
+mkEmptyObject = ghcjsOnly
+#endif
+
+-- string or symbol
+newtype JSKey = JSKey { unJSKey :: JSVal }
+
+class ToJSKey key where
+  toJSKey :: key -> JSM JSKey
+
+instance ToJSKey JSKey where
+  toJSKey = pure
+
+instance ToJSKey String where
+  toJSKey = fmap JSKey . toJSVal
+
+instance ToJSKey Int where
+  toJSKey = fmap JSKey . toJSVal . show
+
+
+getProp :: (ToJSKey key, ToJSObject obj) => key -> obj -> JSM JSVal
+getProp k o = toJSKey k >>= \(JSKey k') -> getPropCoerce k' o
+
+-- | Allows any JSVal as key
+getPropCoerce :: (ToJSVal key, ToJSObject obj) => key -> obj -> JSM JSVal
+#ifdef ghcjs_HOST_OS
+getPropCoerce k o = undefined
+#else
+getPropCoerce = ghcjsOnly
 #endif
 
 
-newtype JSFunction = JSFunction JSVal
+setProp :: (ToJSKey key, ToJSVal val, ToJSObject obj) => key -> val -> obj -> JSM ()
+setProp k v o = toJSKey k >>= \(JSKey k') -> setPropCoerce k' v o
+
+-- | Allows any JSVal as key
+setPropCoerce :: (ToJSVal key, ToJSVal val, ToJSObject obj) => key -> val -> obj -> JSM ()
+#ifdef ghcjs_HOST_OS
+setPropCoerce k v o = do
+  k' <- toJSVal k
+  v' <- toJSVal v
+  o' <- unJSObject <$> toJSObject o
+  setProp_js k' v' o'
+
+foreign import javascript unsafe
+  "$1[$2] = $3" 
+  setProp_js :: JSVal -> JSVal -> JSVal -> JSM ()
+#else
+setPropCoerce = ghcjsOnly
+#endif
+
+
+class ToJSObject o where
+  toJSObject :: o -> JSM JSObject
+
+instance ToJSObject JSObject where toJSObject = pure
+
+
+--------------------------------------------------------------------------------
+-- JSArray
+
+newtype JSArray = JSArray { unJSArray :: JSVal }
+
+instance ToJSObject JSArray where
+  toJSObject = pure . JSObject . unJSArray
+
+jsArrayFromList :: [JSVal] -> JSM JSArray
+#ifdef ghcjs_HOST_OS
+jsArrayFromList xs = do
+  ar <- newArray_js (intToJSVal $ length xs)
+  let ar' = JSObject ar
+  for (zip [0..] xs) $ \(idx, val) -> setProp (idx :: Int) val ar'
+  pure $ JSArray ar
+
+foreign import javascript unsafe
+  "$r = Array.from(Array($1))"
+      -- nb. "pre-allocate" for possible perf boost. unbenchmarked.
+  newArray_js :: JSVal -> JSM JSVal
+#else
+jsArrayFromList = ghcjsOnly
+#endif
+
+jsArrayToList :: JSArray -> JSM [JSVal]
+#ifdef ghcjs_HOST_OS
+jsArrayToList (JSArray ar) = do
+  let ar' = JSObject ar
+  len <- jsValToInt <$> getProp "length" ar'
+  for [0 .. len - 1] $ \idx -> getProp (idx :: Int) ar'
+#else
+jsArrayToList = ghcjsOnly
+#endif
+
+
+--------------------------------------------------------------------------------
+-- JSFunction
+
+newtype JSFunction = JSFunction { unJSFunction :: JSVal }
+
+instance PToJSVal JSFunction where pToJSVal = unJSFunction
+instance ToJSVal JSFunction where toJSVal = pure . unJSFunction
+
+instance ToJSObject JSFunction where toJSObject = pure . JSObject . unJSFunction
+
 
 -- Function type for Haskell/JS interpolation
 type HSFunction =
@@ -90,16 +197,6 @@ mkFun' :: MonadJSM m => ([JSVal] -> JSM ()) -> m JSFunction
 mkFun' fun = mkFun (\_fun _this args -> fun args)
 
 
-#ifdef ghcjs_HOST_OS
--- nb I don't know if these are correct
-intToJSVal :: Int -> JSVal
-intToJSVal = fromIntegral >>> (unsafeCoerce :: Double -> JSVal)
-jsValToInt :: JSVal -> Int
-jsValToInt = (unsafeCoerce :: JSVal -> Double) >>> round
-  -- 'round' seems fishy, but that's what JSaddle uses ...
-#endif
-
-
 setTimeout :: MonadJSM m => Int -> JSFunction -> m Int
 #ifdef ghcjs_HOST_OS
 setTimeout delay (JSFunction fun) =
@@ -126,6 +223,95 @@ clearTimeout = ghcjsOnly
 #endif
 
 
+class ToJSArgs args where
+  toJSArgs :: args -> JSM JSArray
+
+instance ToJSArgs JSVal where
+  toJSArgs = jsArrayFromList . (:[])
+
+instance ToJSVal a => ToJSArgs [a] where
+  toJSArgs = jsArrayFromList <=< traverse toJSVal
+
+instance (ToJSVal a1, ToJSVal a2) => ToJSArgs (a1, a2) where
+  toJSArgs (a1, a2) = do
+    a1' <- toJSVal a1
+    a2' <- toJSVal a2
+    toJSArgs [a1', a2']
+
+
+-- | Call a JS function, safe edition
+-- WANT: a ToProp would be most appropriate since properties can be string|number|symbol
+(#) :: (ToJSObject this, ToJSString prop, ToJSArgs args) => this -> prop -> args -> JSM JSVal
+(#) = (#!)
+infixr 2 #
+
+-- | Call JS function, unsafe edition
+(#!) :: (ToJSObject this, ToJSVal prop, ToJSArgs args) => this -> prop -> args -> JSM JSVal
+#ifdef ghcjs_HOST_OS
+(#!) this prop args = do
+  this' <- unJSObject <$> toJSObject this
+  prop' <- toJSVal prop
+  args' <- unJSArray <$> toJSArgs args
+  unsafeCall_js this' prop' args'
+
+foreign import javascript unsafe
+  "$r = (function (it) { return it[$2].apply(it, $3); })($1)"
+  unsafeCall_js :: JSVal -> JSVal -> JSVal -> JSM JSVal
+#else
+(#!) = ghcjsOnly
+#endif
+infixr 2 #!
+
+
+--------------------------------------------------------------------------------
+-- JSBool
+
+newtype JSBool = JSBool { unJSBool :: JSVal }
+
+instance PToJSVal JSBool where pToJSVal = unJSBool
+instance ToJSVal JSBool where toJSVal = pure . unJSBool
+
+jsTrue, jsFalse :: JSBool
+#ifdef ghcjs_HOST_OS
+jsTrue = JSBool $ unsafePerformIO jsTrue_js
+jsFalse = JSBool $ unsafePerformIO jsFalse_js
+foreign import javascript unsafe "$r = true" jsTrue_js :: JSM JSVal
+foreign import javascript unsafe "$r = false" jsFalse_js :: JSM JSVal
+#else
+jsTrue = ghcjsOnly
+jsFalse = ghcjsOnly
+#endif
+
+
+-------------------------------------------------------------------------------
+
+
+getGlobal :: ToJSString s => s -> JSM JSVal
+#ifdef ghcjs_HOST_OS
+getGlobal k = getGlobal_js =<< toJSVal k
+foreign import javascript unsafe
+  "$r = globalThis[$1]"
+  getGlobal_js :: JSVal -> JSM JSVal
+#else
+getGlobal = ghcjsOnly
+#endif
+
+
+-- nb. Unused at time of writing; remove?
+currentWindowUnsafe :: MonadJSM m => m Window
+currentWindowUnsafe = liftJSM $ unsafeCoerce <$> getGlobal "window"
+
+
+#ifdef ghcjs_HOST_OS
+-- nb I don't know if these are correct
+intToJSVal :: Int -> JSVal
+intToJSVal = fromIntegral >>> (unsafeCoerce :: Double -> JSVal)
+jsValToInt :: JSVal -> Int
+jsValToInt = (unsafeCoerce :: JSVal -> Double) >>> round
+  -- 'round' seems fishy, but that's what JSaddle uses ...
+#endif
+
+
 -- This all is trivial and is included only because S11 re-exports it
 -- WANT: remove it from S11 and then from here
 type JSContextRef = ()
@@ -138,3 +324,12 @@ runJSM act _ = liftIO act
 runJSM = ghcjsOnly
 #endif
 
+eval :: (ToJSString s, MonadJSM m) => s -> m JSVal
+#ifdef ghcjs_HOST_OS
+eval = liftJSM . eval_js . toJSString
+foreign import javascript unsafe
+  "$r = eval($1)"
+  eval_js :: JSString -> IO JSVal
+#else
+eval = ghcjsOnly
+#endif
