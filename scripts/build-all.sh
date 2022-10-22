b340ea33401a5f9e191537c72e2a64659b7cef3e
Maynard <me@maynards.site>
2022-10-21 18:22:38 -0700

    mistake in build script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,5 +1,7 @@
 # run from repo root!
 echo building
+[ -f server ] && rm server
+[ -f client ] && rm client
 nix-build $@ --arg isJS false -o server --arg optimize false & S=$!
 nix-build $@ --arg isJS true -o client --arg optimize false & C=$!
 
